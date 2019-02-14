60e734a81494d6b1da8fbe2e466e5640495f8a1d
Aswathy Kanakarajan <aswathykanakarajan@zipcoders-MacBook-Pro-7.local>
2019-02-14 17:25:21 -0500

    Implemented the Testcases for PhoneBook first and then the Phonebook itself and test all the testcases - All pass

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,56 @@
 package com.zipcodewilmington.phonebook;
 
+import java.util.*;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+    private Map<String, List<String>> phoneMap;
+
+    public PhoneBook(){
+        phoneMap = new HashMap<String, List<String>>();
+    }
+
+    public void add(String name, String phoneNumber) {
+        if(phoneMap.containsKey(name)){
+            List<String> phoneList =phoneMap.get(name);
+            phoneList.add(phoneNumber);
+
+        }else {
+            ArrayList phoneList = new ArrayList<String>();
+            phoneList.add(phoneNumber);
+            phoneMap.put(name, phoneList);
+        }
+    }
+
+    public List<String> lookup(String name) {
+        return phoneMap.get(name);
+    }
+
+    public void remove(String name) {
+        phoneMap.remove(name);
+    }
+
+    public String reverseLookup(String phoneNumber) {
+        String name = "";
+        for (String key : phoneMap.keySet()) {
+            List<String> value = phoneMap.get(key);
+            if(value.contains(phoneNumber)){
+                name = key;
+                break;
+            }
+        }
+        return name;
+    }
+
+    public void display() {
+        for (String key : phoneMap.keySet()) {
+            System.out.print(key + " " );
+            for(String value : phoneMap.get(key)) {
+                System.out.print(value + " " );
+            }
+            System.out.print( "\n" );
+        }
+    }
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,183 @@
 package com.zipcodewilmington.phonebook;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.List;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+
+    @Test
+    public void addTest() {
+        //Given
+        PhoneBook phoneBook = new PhoneBook();
+        String givenName = "Devesh";
+        String givenPhoneNumber = "302-883-4275";
+        //When
+        phoneBook.add(givenName, givenPhoneNumber);
+        //Then
+        List<String> retrievedPhoneNumberList = phoneBook.lookup(givenName);
+        Assert.assertEquals(retrievedPhoneNumberList.get(0),givenPhoneNumber);
+
+    }
+
+    @Test
+    public void addToSameNameTest() {
+        //Given
+        PhoneBook phoneBook = new PhoneBook();
+        String givenName = "Devesh";
+        String givenPhoneNumber = "302-883-4275";
+        String givenPhoneNumber2 = "302-883-4270";
+
+        //When
+        phoneBook.add(givenName, givenPhoneNumber);
+        phoneBook.add(givenName, givenPhoneNumber2);
+        //Then
+        List<String> retrievedPhoneNumberList = phoneBook.lookup(givenName);
+        Assert.assertEquals(retrievedPhoneNumberList.get(0),givenPhoneNumber);
+        Assert.assertEquals(retrievedPhoneNumberList.get(1),givenPhoneNumber2);
+    }
+
+    @Test
+    public void removeTest(){
+        //Given
+        PhoneBook phoneBook = new PhoneBook();
+        String givenName = "Devesh";
+        String givenPhoneNumber = "302-883-4275";
+
+        phoneBook.add(givenName, givenPhoneNumber);
+        //Test add is successful
+        List<String> addedPhoneNumberList = phoneBook.lookup(givenName);
+        Assert.assertEquals(addedPhoneNumberList.get(0),givenPhoneNumber); //
+
+        //When
+        phoneBook.remove(givenName);
+
+        //Then
+        List<String> retrievedPhoneNumber = phoneBook.lookup(givenName);
+        Assert.assertNull(retrievedPhoneNumber); //
+
+    }
+
+    @Test
+    public void lookupTest() {
+        //Given
+        PhoneBook phoneBook = new PhoneBook();
+        String givenName = "Nirmal";
+        String givenPhoneNumber = "302-883-4275";
+
+        //When
+        phoneBook.add(givenName, givenPhoneNumber);
+        //Then
+        List<String> retrievedPhoneNumberList = phoneBook.lookup(givenName);
+        Assert.assertEquals(retrievedPhoneNumberList.get(0),givenPhoneNumber);
+
+    }
+
+    @Test
+    public void lookupWithMoreThanSinglePhoneNumberTest() {
+        //Given
+        PhoneBook phoneBook = new PhoneBook();
+        String givenName = "Nirmal";
+        String givenPhoneNumber = "302-883-4275";
+
+        String givenPhoneNumber2 = "302-883-4270";
+        phoneBook.add(givenName, givenPhoneNumber);
+
+        //When
+        phoneBook.add(givenName, givenPhoneNumber2);
+        //Then
+        List<String> retrievedPhoneNumberList = phoneBook.lookup(givenName);
+        Assert.assertEquals(retrievedPhoneNumberList.get(0),givenPhoneNumber);
+        Assert.assertEquals(retrievedPhoneNumberList.get(1),givenPhoneNumber2);
+
+    }
+    @Test
+    public void reverseLookupTest() {
+
+        //Given
+        PhoneBook phoneBook = new PhoneBook();
+        String givenName = "Nirmal";
+        String givenPhoneNumber = "302-883-4275";
+
+        //When
+        phoneBook.add(givenName, givenPhoneNumber);
+
+        //Then
+        String retrievedName = phoneBook.reverseLookup(givenPhoneNumber);
+        Assert.assertEquals(retrievedName, givenName);
+    }
+
+    @Test
+    public void reverseLookupWhenMoreEntriesInMapTest() {
+
+        //Given
+        PhoneBook phoneBook = new PhoneBook();
+
+        String givenName = "Nirmal";
+        String givenPhoneNumber = "302-883-4275";
+        phoneBook.add(givenName, givenPhoneNumber);
+
+        String givenName2 = "Devesh";
+        String givenPhoneNumber2 = "302-883-4270";
+        phoneBook.add(givenName2, givenPhoneNumber2);
+
+
+        //When
+        String retrievedName = phoneBook.reverseLookup(givenPhoneNumber);
+        //Then
+        Assert.assertEquals(retrievedName, givenName);
+
+    }
+
+    @Test
+    public void reverseLookupWithMoreThanSinglePhoneNumberTest() {
+
+        //Given
+        PhoneBook phoneBook = new PhoneBook();
+
+        String givenName = "Nirmal";
+        String givenPhoneNumber = "302-883-4275";
+        phoneBook.add(givenName, givenPhoneNumber);
+
+        String givenPhoneNumber2 = "302-883-4270";
+        phoneBook.add(givenName, givenPhoneNumber2);
+
+
+        //When
+        String retrievedName = phoneBook.reverseLookup(givenPhoneNumber2);
+        //Then
+        Assert.assertEquals(retrievedName, givenName);
+
+    }
+    @Test
+    public void displayTest() {
+        ByteArrayOutputStream stringOut = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(stringOut);
+        PrintStream oldPrintStream = System.out;
+
+        System.setOut(printStream);
+
+        //Given
+        PhoneBook phoneBook = new PhoneBook();
+        String givenName = "Nirmal";
+        String givenPhoneNumber = "302-883-4275";
+        phoneBook.add(givenName, givenPhoneNumber);
+
+        String givenName2 = "Devesh";
+        String givenPhoneNumber2 = "302-883-4270";
+        phoneBook.add(givenName2, givenPhoneNumber2);
+
+        phoneBook.display();
+        System.out.flush();
+        System.setOut(oldPrintStream);
+
+        String expected = givenName + " "+ givenPhoneNumber + " \n" + givenName2 + " "+ givenPhoneNumber2 + " \n";
+        Assert.assertEquals(stringOut.toString(), expected);
+    }
 }
